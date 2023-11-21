abd201aba21f07fad66f724a8c1b8bad5ad17cb6
Aritra <aritralahiri17@gmail.com>
2023-11-21 22:18:37 +0530

    added logic for delete

--- script.js
+++ script.js
@@ -23,65 +23,68 @@ function addBooking(e) {
   axios
     .post("http://localhost:3000/book-slot", userObj)
     .then((response) => {
+      bookSlotDiv.hidden = true;
+      fetchAllBookingData();
       console.log(response);
     })
     .catch((err) => console.log(err));
 }
 
-// function allStorage() {
-//   var values = [],
-//     keys = Object.keys(localStorage),
-//     i = keys.length;
-
-//   while (i--) {
-//     values.push(JSON.parse(localStorage.getItem(keys[i])));
-//   }
-//   return values;
-// }
-
 function fetchAllBookingData() {
-  axios.get("http://localhost:3000/show-booking").then((response) => {
-    console.log(response.data);
-    for (const user of response.data) {
-      if (user.Users.length > 0) {
-        for (const details of user.Users) {
-          let name = details.Name;
-          let email = details.Email;
-          let time = user.Time;
-          let link = user.Link;
+  axios
+    .get("http://localhost:3000/show-booking")
+    .then((response) => {
+      console.log(response.data);
+      for (const user of response.data) {
+        if (user.Users.length > 0) {
+          for (const details of user.Users) {
+            let name = details.Name;
+            let email = details.Email;
+            let time = user.Time;
+            let link = user.Link;
 
-          let div = document.createElement("div");
-          let innerDiv = document.createElement("div");
-          let a = document.createElement("a");
-          let innerH5 = document.createElement("h5");
-          let innerh6 = document.createElement("h6");
-          //   // Add class
-          div.className = "card col";
-          innerDiv.className = "card-body";
-          innerH5.className = "card-title";
-          innerH5.innerText = name;
-          a.href = link;
-          a.innerText = "JOIN";
-          a.className = "btn btn-primary btn-sm";
-          innerh6.className = "card-subtitle mb-2 text-body-secondary";
-          innerh6.innerText = time;
-          div.style = "width: 15rem;";
-          var bookBtn = document.createElement("button");
-          bookBtn.className = "btn btn-danger btn-sm";
-          bookBtn.appendChild(document.createTextNode("Cancel"));
-          bookBtn.addEventListener("click", function () {
-            alert("Cancelled");
-          });
-          innerDiv.appendChild(innerH5);
-          innerDiv.appendChild(innerh6);
-          div.appendChild(innerDiv);
-          innerDiv.appendChild(bookBtn);
-          innerDiv.appendChild(a);
-          showBooking.appendChild(div);
+            let div = document.createElement("div");
+            let innerDiv = document.createElement("div");
+            let a = document.createElement("a");
+            let innerH5 = document.createElement("h5");
+            let innerh6 = document.createElement("h6");
+            //   // Add class
+            div.className = "card";
+            innerDiv.className = "card-body";
+            innerH5.className = "card-title";
+            innerH5.innerText = name;
+            a.href = link;
+            a.innerText = "JOIN";
+            a.className = "btn btn-primary btn-sm";
+            innerh6.className = "card-subtitle mb-2 text-body-secondary";
+            innerh6.innerText = time;
+            div.style = "width: 15rem;";
+            var bookBtn = document.createElement("button");
+            bookBtn.className = "btn btn-danger btn-sm";
+            bookBtn.appendChild(document.createTextNode("Cancel"));
+            bookBtn.addEventListener("click", function () {
+              console.log(details.id);
+              axios
+                .delete(`http://localhost:3000/delete/${details.id}`)
+                .then((response) => {
+                  div.remove();
+                  console.log(showBooking);
+                  fetchAllBookingData();
+                  console.log(response);
+                })
+                .catch((err) => console.log(err));
+            });
+            innerDiv.appendChild(innerH5);
+            innerDiv.appendChild(innerh6);
+            div.appendChild(innerDiv);
+            innerDiv.appendChild(bookBtn);
+            innerDiv.appendChild(a);
+            showBooking.appendChild(div);
+          }
         }
       }
-    }
-  });
+    })
+    .catch((err) => console.log(err));
 }
 
 function fetchDataFromAPI() {

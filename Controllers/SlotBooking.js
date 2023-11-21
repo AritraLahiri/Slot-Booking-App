6d21a6f834610da965615b4765126158ffdab3a0
Aritra <aritralahiri17@gmail.com>
2023-11-21 22:19:13 +0530

    added logic for cancel slot

--- Controllers/SlotBooking.js
+++ Controllers/SlotBooking.js
@@ -29,28 +29,27 @@ exports.getBookings = (req, res, next) => {
     .catch((err) => console.log(err));
 };
 exports.deleteSlot = (req, res, next) => {
-  const slotId = req.params.slotID;
   const userId = req.params.userID;
-  console.log(slotId);
-  Slot.update(
-    { Remaining: Remaining + 1 },
-    {
-      where: {
-        id: slotId,
-      },
-    }
-  )
-    .then(() => {
-      return User.findByPk(userId);
-    })
+  let slotID;
+  User.findByPk(userId)
     .then((user) => {
       if (!user) {
         res.json([{ Deleted: false }]);
+      } else {
+        slotID = user.dataValues.SlotId;
       }
+
       return user.destroy();
     })
     .then(() => {
-      res.json([{ Deleted: true }]);
+      if (slotID != null) {
+        Slot.findByPk(slotID)
+          .then((slot) => {
+            slot.update({ Remaining: slot.Remaining + 1 });
+          })
+          .catch((err) => console.log(err));
+        res.json([{ Deleted: true }]);
+      }
     })
     .catch((err) => console.log(err));
 };
@@ -62,38 +61,3 @@ exports.getSlots = (req, res, next) => {
     })
     .catch((err) => console.log(err));
 };
-// exports.postAddProduct = (req, res, next) => {
-//   const title = req.body.title;
-//   const imageUrl = req.body.imageUrl;
-//   const price = req.body.price;
-//   const description = req.body.description;
-//   Product.create({
-//     title: title,
-//     price: price,
-//     imageUrl: imageUrl,
-//     description: description,
-//   })
-//     .then(() => res.redirect("/"))
-//     .catch((err) => console.log(err));
-// };
-
-// exports.getEditProduct = (req, res, next) => {
-//   const editMode = req.query.edit;
-//   if (!editMode) {
-//     return res.redirect("/");
-//   }
-//   const prodId = req.params.productId;
-//   Product.findByPk(prodId)
-//     .then((product) => {
-//       if (!product) {
-//         return res.redirect("/");
-//       }
-//       res.render("admin/edit-product", {
-//         pageTitle: "Edit Product",
-//         path: "/admin/edit-product",
-//         editing: editMode,
-//         product: product,
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
